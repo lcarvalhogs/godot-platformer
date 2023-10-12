2083769cfe1b3625a55df3165c4d3115a0d7318a
Luis Augusto de Carvalho <luis.carvalhogs@gmail.com>
2023-10-12 16:05:10 -0300

    Adds initial Game manager and loads level 1 on startup.

---
+++ Game.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+const LVL_PATH = "res://Levels/Level%d.tscn"
+
+var level_number:int = 1
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	call_deferred("init")
+	pass # Replace with function body.
+
+
+func init():
+	load_level(level_number)
+	pass
+
+
+func load_level(num:int):
+	var root = get_tree().root
+	if root.has_node("Level"):
+		root.remove_child($Level)
+	
+	print(LVL_PATH % num)
+	var level = load(LVL_PATH % num).instantiate()
+	root.add_child(level)
+	return true
