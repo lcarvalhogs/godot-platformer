e0c287d4612b34926f8fc8341d31b6c4e3a5c8a5
Luis Augusto de Carvalho <luis.carvalhogs@gmail.com>
2023-10-12 20:00:48 -0300

    Adds door trigger.

--- Triggerables/BaseTriggerable.gd
+++ Triggerables/BaseTriggerable.gd
@@ -5,12 +5,15 @@ extends Area2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	add_to_group("triggerable")
+	connect("body_entered", _on_player_enter)
 
 func trigger(tag_name: String):
-	print("t")
 	if tag.length() == 0 || tag != tag_name:
 		return
 	_on_trigger()
 
 func _on_trigger():
 	print("Need to implement _on_trigger")
+
+func _on_player_enter(body):
+	print("Need to implement on_player_entered")
