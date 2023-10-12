f70b04b5ab792ef10c9fa45d211dab1e5908f68b
Luis Augusto de Carvalho <luis.carvalhogs@gmail.com>
2023-10-12 18:03:01 -0300

    Adds jump fall management.

--- Player.gd
+++ Player.gd
@@ -4,6 +4,7 @@ enum {NORMAL, JUMP, FALL, LAND, DIE}
 const GRAVITY = 800
 const JUMP_VELOCITY = -200
 const AIR_JUMP_MULTIPLIER: float = 0.75
+const COYOTE_TIME: float = 0.1
 
 @export var speed:int = 65
 @export var air_control:bool = true
@@ -11,6 +12,9 @@ const AIR_JUMP_MULTIPLIER: float = 0.75
 
 var state:= NORMAL
 var air_jumps: int = 0
+var jump_start_y: float = 0.0
+var fall_time: float = 0.0
+
 func _ready():
 	pass
 
@@ -20,19 +24,27 @@ func _process(delta):
 
 	match state:
 		NORMAL:
-			horizontal()
-			if Input.is_action_pressed("jump"):
-				air_jumps = max_air_jumps
-				state = JUMP
-				jump()
+			if !is_on_floor():
+				state = FALL
+				jump_start_y = global_position.y
+			else:
+				horizontal()
+				if Input.is_action_pressed("jump"):
+					air_jumps = max_air_jumps
+					state = JUMP
+					jump()
 		JUMP:
 			jump()
 			pass
 		FALL:
 			if is_on_floor():
 				state = LAND
-			pass
+			elif fall_time <= COYOTE_TIME:
+				air_jumps = max_air_jumps
+				jump()
+			fall_time += delta
 		LAND:
+			fall_time = 0
 			state = NORMAL
 			pass
 		DIE:
@@ -70,5 +82,6 @@ func jump():
 			velocity.y = JUMP_VELOCITY * AIR_JUMP_MULTIPLIER
 
 		air_jumps -= 1
+		jump_start_y = global_position.y
 	if is_on_floor() and velocity.y >= 0:
 		state = LAND
