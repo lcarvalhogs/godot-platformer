6aadc1e3aa85daea6084ee3a6db939aedbf4f267
Luis Augusto de Carvalho <luis.carvalhogs@gmail.com>
2023-10-12 17:20:24 -0300

    Adds horizontal movement to player and setup input keys.

--- Player.gd
+++ Player.gd
@@ -1,13 +1,31 @@
 extends CharacterBody2D
 
 const GRAVITY = 800
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var speed:int = 65
+@export var air_control:bool = true
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+func _ready():
+	pass
 
+func _process(delta):
+	horizontal()
 
 func _physics_process(delta):
 	velocity.y += GRAVITY * delta
 	move_and_slide()
+
+func horizontal():
+	if Input.is_action_pressed("right"):
+		velocity.x = speed
+		$Sprite2D.flip_h = false
+	elif Input.is_action_pressed("left"):
+		velocity.x = -speed
+		$Sprite2D.flip_h = true
+	elif is_on_floor() or air_control:
+		velocity.x = 0
+
+	if is_on_floor():
+		if velocity.x == 0:
+			$AnimationPlayer.play("idle")
+		else:
+			$AnimationPlayer.play("walk")
