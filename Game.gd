d12d2fd7b20b020b32fbca4d5ff11ca510e7676b
Luis Augusto de Carvalho <luis.carvalhogs@gmail.com>
2023-10-14 12:14:15 -0300

    Adds computer triggerable and logic to trigger and replace blocks.

--- Game.gd
+++ Game.gd
@@ -15,16 +15,21 @@ func init():
 
 
 func load_level(num:int):
+	var old_level = get_level_node()
 	var root = get_tree().root
-	if root.has_node("Level"):
-		var current_level = root.get_node("Level")
-		root.remove_child(current_level)
-		current_level.queue_free()
+	if old_level != null:
+		root.remove_child(old_level)
+		old_level.queue_free()
 	
-	print(LVL_PATH % num)
 	var level = load(LVL_PATH % num).instantiate()
 	root.add_child(level)
 	return true
+
+func get_level_node():
+	var root = get_tree().root
+	if root.has_node("Level"):
+		return root.get_node("Level")
+	return null
 
 # Game group functions
 
@@ -38,3 +43,7 @@ func on_pickup(item):
 	print(item.name)
 	if item.name == "Key":
 		get_tree().call_group("triggerable", "trigger", "Door")
+
+func on_computer():
+	var level: Level = get_level_node()
+	level.replace_tiles(level.BLOCK_OUTLINE, level.BLOCK)
